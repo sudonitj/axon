895470ec611f01f3e675f52d3674ba78cb2da3de
RishiAhuja <www.rishiahuja@gmail.com>
2025-03-10 01:08:53 +0530

    refactor(.): proper directory structore and cmakelists.txt

---
+++ build/CMakeFiles/a.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a.dir/lib/utils/fileio.c.o"
+  "CMakeFiles/a.dir/lib/utils/fileio.c.o.d"
+  "CMakeFiles/a.dir/src/main.c.o"
+  "CMakeFiles/a.dir/src/main.c.o.d"
+  "a"
+  "a.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/a.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
